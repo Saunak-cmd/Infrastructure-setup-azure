e482cad784ca87fc51358de50fabe8999c2ed0e3
saunakdevops <saunakdevops@gmail.com>
2024-12-03 09:05:32 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,52 +1,44 @@
 provider "azurerm" {
   features {}
-  subscription_id = "f027d2e5-7ece-41c4-8a19-54dfa1d3491d"  # Replace with your Azure Subscription ID
+  subscription_id = var.subscription_id
 }
-
 terraform {
   backend "azurerm" {
-    resource_group_name   = "RG-saunak"
-    storage_account_name  = "saunak021224"
-    container_name        = "container-saunak"
-    key                   = "terraform2.tfstate"
+   resource_group_name   = "RG-Swati"
+   storage_account_name  = "swati21224"
+    container_name        = "container1"
+   key                   = "terraform2.tfstate"
   }
-}
-
+  }
 # Reference the existing resource group
 data "azurerm_resource_group" "existing_rg" {
-  name = "RG-saunak"  # Replace with your actual existing resource group name
+  name = var.existing_resource_group_name
 }
-
 resource "azurerm_container_registry" "container" {
-  name                = "registrysaunak"  # Replace with your ACR name
+  name                = var.acr_name
   resource_group_name = data.azurerm_resource_group.existing_rg.name
   location            = data.azurerm_resource_group.existing_rg.location
-  sku                  = "Basic"  # Replace with the ACR SKU you want (Basic, Standard, Premium)
+  sku                  = var.acr_sku
   admin_enabled       = true
-
-  tags = {
-    environment = "dev"  # Optional: Replace with your tags
-  }
+ 
+  tags = var.tags
 }
-
+ 
 resource "azurerm_kubernetes_cluster" "example_kc" {
-  name                = "cluster-saunak"  # Replace with your AKS cluster name
+  name                = var.aks_name
   location            = data.azurerm_resource_group.existing_rg.location
   resource_group_name = data.azurerm_resource_group.existing_rg.name
-  dns_prefix          = "myaks"  # Replace with your DNS prefix for the AKS cluster
-
+  dns_prefix          = var.aks_dns_prefix
+ 
   default_node_pool {
     name       = "default"
-    node_count = 3  # Replace with your desired node count
-    vm_size    = "Standard_DS2_v2"  # Replace with your desired VM size
+    node_count = var.node_count
+    vm_size    = var.vm_size
   }
-
+ 
   identity {
     type = "SystemAssigned"
   }
-
-  tags = {
-    environment = "dev"
-    owner = "saunak  "  # Optional: Replace with your tags
-  }
+ 
+  tags = var.tags
 }
